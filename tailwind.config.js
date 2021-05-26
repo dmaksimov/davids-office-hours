46ebbefcd19b80fdfd5460310d1e81acb7d2940a
David Maksimov <dmaksi@gmail.com>
2021-05-25 21:16:07 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,65 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans-primary': ['Roboto', 'sans-serif'],
+      'sans-secondary': ['Raleway'],
+      'sans-body-alternative': ['Barlow', 'Roboto', 'sans-serif'],
+    },
+    colors: {
+      orange: {
+        DEFAULT: '#ec7423'
+      },
+      white: colors.white,
+      blue: {
+        light: '#c5dfff',
+      },
+      dark: {
+        DEFAULT: '#383838',
+      }
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'splash': "url('/images/top-bg.jpg')",
+        'cta-pattern': "url('/images/cta-bg.png')",
+        'code-illustration': "url('/images/code-illustration.png')",
+      }),
+
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1280px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: 0,
+        lg: 0,
+        xl: 0,
+        '2xl': 0,
+      },
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
